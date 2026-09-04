05550271a2917efc1fcd204fd163e921590da975
Nikhil Singh <nikhilsingh.cse07@gmail.com>
2026-09-04 23:16:54 +0530

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
@@ -5,11 +6,16 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages base path
+  base: '/AI-BASED-DOCUMENT-SCANNING/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
